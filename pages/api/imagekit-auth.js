284a2c896b332db3e0e5960d08bc41b1f28b5444
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-06-22 04:20:42 +0700

    update: modify imagekit-auth

---
+++ pages/api/imagekit-auth.js
@@ -0,0 +1,16 @@
+// pages/api/imagekit-auth.js
+import imagekit from "@/lib/imagekit";
+
+export default function handler(req, res) {
+  try {
+    const authParams = imagekit.getAuthenticationParameters();
+    console.log("Auth Params:", authParams);
+
+    res.status(200).json(authParams);
+  } catch (error) {
+    console.error("Error generating ImageKit auth params:", error);
+    res
+      .status(500)
+      .json({ message: "Failed to generate ImageKit auth params" });
+  }
+}
