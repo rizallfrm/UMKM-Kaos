3c7ff60d9dfbb62ffdd81d6305f2d2a8c3610cb0
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-06-22 04:19:22 +0700

    update: modify _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import { ApolloProvider } from '@apollo/client';
-import { useApollo } from '../lib/apolloClient';
-import { SessionProvider } from 'next-auth/react';
-import '../styles/globals.css';
+import { ApolloProvider } from "@apollo/client";
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
 
+import { apolloClient } from "../lib/apolloClient";
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
-  const apolloClient = useApollo(pageProps);
+
 
   return (
     <SessionProvider session={session}>
