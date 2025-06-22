35e97e5374421dc7353fc9ef091d4e13705db549
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-06-23 03:14:23 +0700

    update: modify schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -10,6 +10,7 @@ export const typeDefs = gql`
     sizes: [String!]!
     colors: [String!]!
     createdAt: String!
+    updatedAt: String!
   }
 
   type User {
@@ -17,15 +18,40 @@ export const typeDefs = gql`
     email: String!
     name: String!
     role: String!
+    createdAt: String!
+    updatedAt: String!
+    lastLogin: String
+    isActive: Boolean!
+  }
+
+  type AuthPayload {
+    token: String!
+    user: User!
   }
 
   type Query {
-    products(limit: Int, orderBy: String, orderDirection: String): [Product!]!
+    # Product Queries
+    products(
+      limit: Int
+      orderBy: String
+      orderDirection: String
+      search: String
+    ): [Product!]!
     product(id: ID!): Product
+    featuredProducts(limit: Int = 4): [Product!]!
+
+    # User Queries
     users: [User!]!
+    user(id: ID!): User
+    me: User
+
+    # Analytics
+    productCount: Int!
+    userCount: Int!
   }
 
   type Mutation {
+    # Product Mutations
     createProduct(
       name: String!
       description: String!
@@ -44,5 +70,38 @@ export const typeDefs = gql`
       colors: [String]
     ): Product!
     deleteProduct(id: ID!): Boolean
+
+    # User Mutations
+    createUser(
+      email: String!
+      name: String!
+      role: String = "user"
+      password: String!
+    ): User!
+    updateUser(
+      id: ID!
+      email: String
+      name: String
+      role: String
+      isActive: Boolean
+    ): User!
+    deleteUser(id: ID!): Boolean
+    changePassword(currentPassword: String!, newPassword: String!): Boolean
+
+    # Auth Mutations
+    login(email: String!, password: String!): AuthPayload!
+    register(email: String!, name: String!, password: String!): AuthPayload!
+    forgotPassword(email: String!): Boolean
+    resetPassword(token: String!, newPassword: String!): Boolean
+  }
+
+  type Subscription {
+    # Real-time updates
+    productCreated: Product!
+    productUpdated: Product!
+    productDeleted: ID!
+    userCreated: User!
+    userUpdated: User!
+    userDeleted: ID!
   }
 `;
