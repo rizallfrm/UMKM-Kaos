c9ee881ef7ce7b2e6caa50f220374096a70e9912
Rizal Firmansyah <rizalfirmansyah2105@gmail.com>
2025-06-26 17:40:25 +0700

    update: modify pages.index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { GET_FEATURED_PRODUCTS } from "../graphql/queries";
+import { GET_FEATURED_PRODUCTS, GET_PRODUCT_REVIEWS } from "../graphql/queries";
 import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
 import "../styles/globals.css";
@@ -9,8 +9,25 @@ import { useRouter } from "next/router";
 import { useMutation } from "@apollo/client";
 import { CREATE_PRODUCT } from "../graphql/queries";
 import { ChevronLeft, LayoutDashboard, Menu, Package, X } from "lucide-react";
+import ProductRating from "@/components/ProductRating";
 
-export default function HomePage() {
+export default function HomePage({ productId }) {
+  const { dataRev, loadingRev, errorRev } = useQuery(GET_PRODUCT_REVIEWS, {
+    variables: { productId },
+  });
+
+  if (loadingRev) return <p>Loading...</p>;
+  if (errorRev) return <p>Error loading reviews</p>;
+
+  const reviews = dataRev?.getProductReviews || [];
+
+  const averageRating =
+    reviews.length > 0
+      ? (
+          reviews.reduce((total, r) => total + r.rating, 0) / reviews.length
+        ).toFixed(1)
+      : null;
+
   const { data: session, status } = useSession();
 
   const { loading, error, data } = useQuery(GET_FEATURED_PRODUCTS, {
@@ -446,16 +463,9 @@ export default function HomePage() {
                   <h3 className="font-bold text-lg text-gray-900 group-hover:text-purple-600 transition-colors duration-200 line-clamp-1">
                     {product.name}
                   </h3>
-                  <div className="flex items-center space-x-1">
-                    <svg
-                      className="w-4 h-4 text-yellow-400"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                    </svg>
-                    <span className="text-xs text-gray-500">4.8</span>
-                  </div>
+
+                  {/* rating */}
+                  <ProductRating productId={product.id} />
                 </div>
 
                 <p className="text-gray-600 text-sm mb-4 line-clamp-2 leading-relaxed">
